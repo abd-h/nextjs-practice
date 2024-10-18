32c28685096edb75a0fa6eadb715e6fd8f538d48
Abdalla Hussein <a.hussein1934@gmail.com>
2024-10-18 20:55:37 +0100

    restructured folder

---
+++ nextjs-intro/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EC/Yz/cTRJ5s1JytZ4zK+iqu9v4TBkUMpM70mglFOCA=\"\n}"
---
+++ nextjs-intro/app/blog/[slug]/page.js
@@ -0,0 +1,9 @@
+const BlogPostPage = ({ params }) => {
+    console.log(params);
+    return <main>
+        <h1>Blog Post</h1>
+        <p>{ params.slug }</p>
+    </main>
+};
+
+export default BlogPostPage;
