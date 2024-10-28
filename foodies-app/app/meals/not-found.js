7f83604e1c1da15f2be8fab8c652a10c898884b0
Abdalla Hussein <a.hussein1934@gmail.com>
2024-10-28 21:45:59 +0000

    added

--- foodies-app/app/meals/not-found.js
+++ foodies-app/app/meals/not-found.js
@@ -4,7 +4,7 @@ const Notfound = () => {
   return (
     <main className='not-found'>
       <h1>Meal Not found!</h1>
-      <p>Unfortunately we could not found requested page or Meal Data!</p>
+      <p>Unfortunately we could not found requested page or Meal Data!!</p>
     </main>
   );
 };
