39d98e50d430f362a77db893ee54be52f08dd33b
Abdalla Hussein <a.hussein1934@gmail.com>
2024-10-28 20:55:40 +0000

    Rendering & Loading
    Rendering and Loading Meal Details via Dynamic Routes and Route Parameters

---
+++ foodies-app/app/meals/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+const Error = () => {
+    return (
+        <main className='error'>
+            <h1>An error occurred!</h1>
+            <p>failed to fetch data, please try again!</p>
+        </main>
+    )
+}
+
+export default Error;
---
+++ foodies-app/app/not-found.js
@@ -0,0 +1,12 @@
+// to catch all none found errors
+
+const Notfound = () => {
+  return (
+    <main className='not-found'>
+      <h1>Not found</h1>
+      <p>Unfortunately we could not found requested page or resource!</p>
+    </main>
+  );
+};
+
+export default Notfound;
