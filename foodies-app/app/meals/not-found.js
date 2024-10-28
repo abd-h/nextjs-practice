ff5661945086d4d9115f9e9e6723251214904517
Abdalla Hussein <a.hussein1934@gmail.com>
2024-10-28 21:59:54 +0000

    Throwing not found Errors
    Throwing notFound errors for individual meals

---
+++ foodies-app/app/meals/not-found.js
@@ -0,0 +1,12 @@
+// to catch all none found errors
+
+const Notfound = () => {
+  return (
+    <main className='Meal not-found'>
+      <h1>Meal Not found</h1>
+      <p>Unfortunately we could not found requested page or Meal data!</p>
+    </main>
+  );
+};
+
+export default Notfound;
