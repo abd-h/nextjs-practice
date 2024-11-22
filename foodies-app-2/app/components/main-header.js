182feb11acd6aa0f2041df2cbc07f274e5e68752
Abdalla Hussein <a.hussein1934@gmail.com>
2024-11-22 23:15:13 +0000

    Added css active class dynamiclly on links

--- foodies-app-2/app/components/main-header.js
+++ foodies-app-2/app/components/main-header.js
@@ -1,9 +1,17 @@
+'use client'
 const { default: Link } = require("next/link");
+import { usePathname } from 'next/navigation';
 import Image from "next/image";
 import classes from "./main-header.module.css";
 import logoImage from "@/assets/icons/meal.png";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from './nav-link';
+
+
 const MainHeader = () => {
+    const path = usePathname();
+    console.log(path);
+
   return (
     <>
       <MainHeaderBackground />
@@ -15,10 +23,14 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals" >
+                Browse Meals
+              </NavLink>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community" >
+                Community
+              </NavLink>
             </li>
           </ul>
         </nav>
